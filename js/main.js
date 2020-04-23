bc63c5b830f74d7198c2759e640113065c261d0c
Jasmin Voigtlander <jasminvoigtlander@Jasmins-Air.attlocal.net>
2020-04-22 18:46:56 -0700

    Added contact form and contact section styling changes

--- js/main.js
+++ js/main.js
@@ -81,6 +81,21 @@ function main() {
 }
 main();
 
-function sendMessage() {
-    window.location.href = 'mailto:jasminnvoigt@gmail.com';
-}
+// Send Message
+var myform = $("form#myform");
+myform.submit(function (event) {
+    event.preventDefault();
+
+    var service_id = "default_service";
+    var template_id = "portfolio_template";
+
+    emailjs.sendForm(service_id, template_id, myform[0])
+        .then(function () {
+            alert("Message sent!");
+            myform.find("button").text("Send");
+        }, function (err) {
+            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+            myform.find("button").text("Send");
+        });
+    return false;
+});
